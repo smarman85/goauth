49c494048dc0bd3caeaecd4ce5e1b146ec3598bc
Sean <seanmarman@gmail.com>
2020-09-20 17:12:47 -0700

    bcrypt and updates

--- json/main.go
+++ json/main.go
@@ -14,6 +14,7 @@ func main() {
   // create server on 8087
   http.HandleFunc("/encode", foo)
   http.HandleFunc("/decode", bar)
+  http.HandleFunc("/decode2", bar2)
   http.ListenAndServe(":8087", nil)
 }
 
@@ -23,18 +24,37 @@ func foo(w http.ResponseWriter, r *http.Request) {
     First: "Sean",
   }
 
-  err := json.NewEncoder(w).Encode(&p1)
+  p2 := person{
+    First: "Tiffany",
+  }
+
+  people := []person{p1, p2}
+
+  err := json.NewEncoder(w).Encode(people)
   if err != nil {
     log.Println("Encoded bad data", err)
   }
+
+
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
   //curl -XGET -H "Content-type: application/json" -d '{"First": "Tiffany"}' 'localhost:8087/decode'
+  //people := []person{}
   var p1 person
   err := json.NewDecoder(r.Body).Decode(&p1)
   if err != nil {
     log.Println("Decoded bad data", err)
   }
   log.Println("Person:", p1)
+}
+
+func bar2(w http.ResponseWriter, r *http.Request) {
+  //curl -XGET -H "Content-type: application/json" -d '[{"First": "Katelyn"},{"First": "Hailey"}]' localhost:8087/decode
+  people := []person{}
+  err := json.NewDecoder(r.Body).Decode(&people)
+  if err != nil {
+    log.Println("bad data sent to bar2", err)
+  }
+  log.Println("People:", people)
 }
